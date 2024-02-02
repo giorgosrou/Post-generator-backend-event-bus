70d8f10cfaaf95cd5b629986f8282f1ed0c481ee
Georgios Rounis <georg.rounis@gmail.com>
2024-02-02 18:16:51 +0100

    implemented comments event bus call

--- posts/index.js
+++ posts/index.js
@@ -6,6 +6,7 @@ const cors = require ('cors');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+const axios = require('axios');
 
 const posts = {};
 
@@ -13,13 +14,22 @@ app.get('/posts', (req,res) => {
     res.send(posts);
 });
 
-app.post('/posts', (req,res) => {
+app.post('/posts', async(req,res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
+
     posts[id] = {
         id,
         title
     }
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id,title
+        }
+    });
+
     res.status(201).send(posts[id]);
 });
 
